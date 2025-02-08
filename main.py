5b9b375213c545f441836879de17bbc9f1bc73bc
bonilla006 <151087964+bonilla006@users.noreply.github.com>
2025-02-08 11:39:14 -0400

    inicio del proyecto de sv para mi reina<3

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/frente_casa/')
+def casa():
+    return render_template('puerta.html')
+
+@app.route('/dentro_casa/')
+def encasa():
+    return render_template('encasa.html')
+
+@app.route('/mesa/')
+def mesa():
+    return render_template('mesa.html')
+
+@app.route('/carta/')
+def carta():
+    return render_template('carta.html')
+
+@app.route('/aventura/')
+def espacio():
+    return render_template('espacio.html')
+
+@app.route('/triste/')
+def espacio_triste():
+    return render_template('espacio_triste.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
